08bf28a39fb88827bbb96ac1757d4e0033da52f1
Hasan Mostafa <hasanalimostafa1@gmail.com>
2026-02-13 04:55:57 +0300

    Add a mock database

---
+++ db.js
@@ -0,0 +1,17 @@
+// db.js
+
+const authors = [
+  { id: 1, name: "Bryan" },
+  { id: 2, name: "Christian" },
+  { id: 3, name: "Jason" },
+];
+
+async function getAuthorById(authorId) {
+  return authors.find((author) => author.id === authorId);
+}
+
+async function getAllAuthors() {
+  return authors;
+}
+
+module.exports = { getAuthorById, getAllAuthors };
